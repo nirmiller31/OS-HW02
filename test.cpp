7267aa647bb0fb96a5267454e6565f041ff843b3
Nir Miller <nir.miller@campus.technion.ac.il>
2025-05-21 21:25:29 +0300

    [HW02] wide fork test

--- test.cpp
+++ test.cpp
@@ -20,6 +20,8 @@
 #define FOURTH_FUNC_FLIP_SEC_BRANCH 337
 
 #define SHORT_TEST_ITERATIONS       20
+#define MEDIUM_TEST_ITERATIONS      70
+#define LONG_TEST_ITERATIONS        120
 
 template<typename T, std::size_t N>
 std::ostream& operator<<(std::ostream& os, const std::array<T, N>& arr) {
@@ -72,9 +74,9 @@ bool verify_default_is_zero_test() {
 
 }
 
-bool verify_simple_setter_getter() {
+bool verify_simple_setter_getter_test() {
 
-         for(int i=0 ; i<SHORT_TEST_ITERATIONS ; i++) {
+         for(int i=0 ; i<MEDIUM_TEST_ITERATIONS ; i++) {
 
                   bool print_enable = false;
 
@@ -87,7 +89,7 @@ bool verify_simple_setter_getter() {
                   long returned = syscall(FIRST_FUNC_SET_SEC, array[0], array[1], array[2], array[3], array[4]);
                   if(print_enable) std::cout << "SysCall  SET_SEC returned: " << returned << std::endl;
 
-                  if(returned < 0) return 0;
+                  if(returned < 0) {return 0; }
 
                   result_array[0] = syscall(SECOND_FUNC_GET_SEC, LETTER_S_SWORD);
                   result_array[1] = syscall(SECOND_FUNC_GET_SEC, LETTER_M_MIDNIGHT);
@@ -97,13 +99,73 @@ bool verify_simple_setter_getter() {
 
                   if(print_enable) std::cout << "I got " << array << " for the verify_simple_setter_getter_test" << std::endl;
 
-                  if(result_array != array) return 0;
+                  if(result_array != array) {return 0; }
 
                   if(print_enable) std::cout << "verify_simple_setter_getter_test Passed for the " << i << "time" << std::endl;
-                  if(print_enable) std::cout << "-------------------------------------------------------------------" << std::endl;
-                  return 1;
+                  if(print_enable) std::cout << "-------------------------------------------------------------------" << std::endl;        
          }
+         return 1;
+}
 
+bool verify_wide_fork_setter_getter_test() {
+
+         for(int i=0 ; i<SHORT_TEST_ITERATIONS ; i++) {
+
+                  bool print_enable = false;
+
+                  std::array<int, 5> array = generate_5_array();;
+                  std::array<int, 5> result_array;
+                  std::array<int, 5> child_result_array;
+
+                  if(print_enable) std::cout << "----------------------------------------------------------------------" << std::endl;
+                  if(print_enable) std::cout << "Im setting " << array << " for the verify_wide_fork_setter_getter_test" << std::endl;
+                  
+                  long returned = syscall(FIRST_FUNC_SET_SEC, array[0], array[1], array[2], array[3], array[4]);
+                  if(print_enable) std::cout << "SysCall  SET_SEC returned: " << returned << std::endl;
+
+                  if(returned < 0) return 0;
+
+                  result_array[0] = syscall(SECOND_FUNC_GET_SEC, LETTER_S_SWORD);
+                  result_array[1] = syscall(SECOND_FUNC_GET_SEC, LETTER_M_MIDNIGHT);
+                  result_array[2] = syscall(SECOND_FUNC_GET_SEC, LETTER_C_CLAMP);
+                  result_array[3] = syscall(SECOND_FUNC_GET_SEC, LETTER_D_DUTY);
+                  result_array[4] = syscall(SECOND_FUNC_GET_SEC, LETTER_I_ISOLATE);
+
+                  if(print_enable) std::cout << "I got " << result_array << " for the verify_wide_fork_setter_getter_test" << std::endl;
+
+                  if(result_array != array) return 0;
+
+                  for(int j=0 ; j<MEDIUM_TEST_ITERATIONS ; j++){
+                           
+                           pid_t pid = fork();                 // Fork to verify we inherit the clearance field
+
+                           if(pid == 0) {                      // Child process, verify the clearance field is as written
+                                    child_result_array[0] = syscall(SECOND_FUNC_GET_SEC, LETTER_S_SWORD);
+                                    child_result_array[1] = syscall(SECOND_FUNC_GET_SEC, LETTER_M_MIDNIGHT);
+                                    child_result_array[2] = syscall(SECOND_FUNC_GET_SEC, LETTER_C_CLAMP);
+                                    child_result_array[3] = syscall(SECOND_FUNC_GET_SEC, LETTER_D_DUTY);
+                                    child_result_array[4] = syscall(SECOND_FUNC_GET_SEC, LETTER_I_ISOLATE);
+                                    if(print_enable) std::cout << "I got " << child_result_array << " for the verify_wide_fork_setter_getter_test" << std::endl;
+                                    if(child_result_array != array) _exit(1);             // FAIL
+                                    _exit(0);                                             // SUCCESS
+                           }
+                           if(pid > 0) {                       // Parent process, just wait the child
+                                    int status;
+                                    waitpid(pid, &status, 0);
+                                    if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {return 0; }
+                                    if(print_enable) std::cout << "Just Verified " << pid << " 's clearance field" << std::endl;
+                           }
+                           if(pid < 0) {
+                                    if(print_enable) std::cout << "Unexpected ERROR, verify_wide_fork_setter_getter_test failed" << std::endl;
+                                    return 0;                  // Fork failed
+                           }
+                  }
+
+                  if(print_enable) std::cout << "verify_wide_fork_setter_getter_test Passed for the " << i << "time" << std::endl;
+                  if(print_enable) std::cout << "-------------------------------------------------------------------" << std::endl;
+                  
+         }
+         return 1;
 }
 
 
@@ -112,7 +174,9 @@ int main() {
          srand(static_cast<unsigned int>(time(0))); // Seed for randomization
 
          run_test("Default clearance is zero", verify_default_is_zero_test);
-         run_test("Simple Setter Getter test", verify_simple_setter_getter);
+         run_test("Simple Setter Getter test", verify_simple_setter_getter_test);
+         run_test("Wide Fork Setter Getter test", verify_wide_fork_setter_getter_test);
+         run_test("Deep Fork Setter Getter test", verify_wide_fork_setter_getter_test);
 
          std::cout << "\nTest Summary:\n";                                                         // Summary
          if (failed_tests.empty()) {
